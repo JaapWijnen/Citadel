e2c3176e1b66dec483d21feb5471e56487a683d8
Gwynne Raskind <gwynne@darkrainfall.org>
2020-04-21 18:25:24 -0500

    The set of changes so far would be very difficult to break into multiple commits; apologies for the monster commit. Some highlights: - Most things in SSHChannel.swift have doc comments now - The magic numbers for the channel config are now named constants - SSHChannelResponse has been split into the two packet types (which now live in SSHCHannelPacketTypes.swift) with somewhat more precise names. - Removed a handful of totally unreferenced things, such as the `unopened` state on SSHForwardedChannel. - Tried to clean up access control for various types and methods, success is questionable - Reorganized the SSHForwardedChannel implementation a little to group things a bit more strongly. - Redid the `handle(packet:)` function in `SSHSession.openChannel()` about six dozen times; I'm a bit ashamed of the ridiculous amount of time I spent on it, only to in the end put most of the logic back where it started (more or less). I did learn better just how much of a pain in the arse Result<> can be to actually use, though... Also, the necessity of ByteBuffer being mutable to be read is a travesty. - Moved the types related specifically to channel packets to their own file.

--- Sources/Citadel/SSHChannel.swift
+++ Sources/Citadel/SSHChannel.swift
@@ -1,23 +1,63 @@
 import NIO
 
-final class SSHForwardedChannel {
-    enum State {
-        case unopened, closed
-        case open(SSHChannelResponse.Success)
+
+// MARK: - Error types
+
+/// Errors that may be thrown during various `SSHChannel` operations
+enum SSHChannelError: Error {
+    case unsupported, isClosed, cannotCloseUnopenedChannel
+}
+
+
+// MARK: - Constants/configuration
+
+/// - Note: Normally I'd put these statically on an appropriate type, but I couldn't figure out
+///   the best one so I just left them global.
+
+/// Forwarded SSH channel window size.
+fileprivate let SSH_CHANNEL_WINDOW_SIZE: UInt32 = 1 << 21 // 2^21 == 2_097_152 == 2MiB
+
+/// Maximum packet size for forwarded SSH channel.
+fileprivate let SSH_CHANNEL_MAX_PACKET_SIZE: UInt32 = 1 << 15 // 2^15 == 32_768 == 32KiB
+
+/// Maximum number of bytes to write to a channel at once.
+fileprivate let SSH_CHANNEL_MAX_WRITE = Int(SSH_CHANNEL_MAX_PACKET_SIZE) - 64
+
+
+/// Sigh, Swift needs a `#assert`, as put forth here:
+///     https://forums.swift.org/t/compile-time-constant-expressions-for-swift/12879
+/// Why can't they focus on this instead of trailing closure syntax?
+///
+/// For now, this is invoked from `SSHSession.forward()`, which is really not at all preferable,
+/// but is the best as can be done at present.
+fileprivate let _assert_sanity: Void = { () -> Void in
+    /// Make sure the config params are at least minimally sensible.
+    /// - TODO: I made some basic assumptions about the requirements of these values.
+    ///   If any of the assumptions are wrong, just yank the assertions.
+    assert(SSH_CHANNEL_WINDOW_SIZE.nonzeroBitCount == 1, "Window size must be a power of two")
+    assert(SSH_CHANNEL_MAX_PACKET_SIZE.nonzeroBitCount == 1, "Max packet size should really be a power of two")
+    assert(SSH_CHANNEL_MAX_WRITE + 32 < SSH_CHANNEL_MAX_PACKET_SIZE, "Max write can't be within 32 bytes of the max packet size")
+}()
+
+
+// MARK: - SSHForwardedChannel
+
+/// An NIO-based channel that forwards data over an SSH connection.
+public final class SSHForwardedChannel {
+    
+    public var isOpen: Bool { self.state != .closed } // `self.state == .open(_)` doesn't work
+    
+    /// Current channel state and SSH connection control data.
+    fileprivate enum State: Equatable {
+        case closed
+        case open(SSHChannelOpenChannelInfo)
     }
     
-    let session: SSHSession
     private var state: State
-    public var isOpen: Bool {
-        switch state {
-        case .open:
-            return true
-        case .unopened, .closed:
-            return false
-        }
-    }
+    private let session: SSHSession
+    private var _pipeline: ChannelPipeline!
+    
     fileprivate let closePromise: EventLoopPromise<Void>
-    fileprivate private(set) var _pipeline: ChannelPipeline!
     
     fileprivate init(session: SSHSession, state: State) {
         self.session = session
@@ -44,25 +84,23 @@ final class SSHForwardedChannel {
         }
     }
     
-    func read(_ data: ByteBuffer) {
+    fileprivate func read(_ data: ByteBuffer) {
         pipeline.fireChannelRead(NIOAny(data))
     }
     
-    fileprivate func write(_ data: ByteBuffer) -> EventLoopFuture<Void> {
-        guard case .open(let state) = state else {
+    fileprivate func write(_ outgoingData: ByteBuffer) -> EventLoopFuture<Void> {
+        guard case .open(let state) = self.state else {
             return eventLoop.makeFailedFuture(SSHChannelError.isClosed)
         }
         
-        var data = data
+        var buffer = outgoingData
         var future = eventLoop.makeSucceededFuture(())
         
-        while data.readableBytes > 0 {
-            guard
-                let data = data.readSlice(length: min(32_700, data.readableBytes))
-            else {
-                return session.channel.eventLoop.makeSucceededFuture(())
+        while buffer.readableBytes > 0 {
+            guard let data = buffer.readSlice(length: Swift.min(SSH_CHANNEL_MAX_WRITE, buffer.readableBytes)) else {
+                fatalError("Failure to readSlice(), corrupted ByteBuffer?")
             }
-        
+            
             future = future.flatMap {
                 self.session.channel.writeAndFlush(
                     SSHClientMessage.channelSend(channel: state.recipientId, data: data)
@@ -74,47 +112,50 @@ final class SSHForwardedChannel {
     }
 }
 
-enum SSHChannelError: Error {
-    case unsupported, isClosed, cannotCloseUnopenedChannel
-}
-
+/// NIO Channel conformance for `SSHForwardedChannel`. Pretty much just forwards
+/// everything to the implementations on the main class.
 extension SSHForwardedChannel: Channel, ChannelCore {
-    func localAddress0() throws -> SocketAddress {
-        throw SSHChannelError.unsupported
+    
+    // Unsupported operations
+    public var localAddress: SocketAddress? { nil }
+    public var remoteAddress: SocketAddress? { nil }
+    public func localAddress0() throws -> SocketAddress { throw SSHChannelError.unsupported }
+    public func remoteAddress0() throws -> SocketAddress { throw SSHChannelError.unsupported }
+    public func bind0(to: SocketAddress, promise: EventLoopPromise<Void>?) { promise?.fail(SSHChannelError.unsupported) }
+    public func connect0(to: SocketAddress, promise: EventLoopPromise<Void>?) { promise?.fail(SSHChannelError.unsupported) }
+    public func triggerUserOutboundEvent0(_: Any, promise: EventLoopPromise<Void>?) { promise?.fail(SSHChannelError.unsupported) }
+    public func setOption<O: ChannelOption>(_: O, value: O.Value) -> EventLoopFuture<Void> { eventLoop.makeFailedFuture(SSHChannelError.unsupported) }
+    public func getOption<O: ChannelOption>(_: O) -> EventLoopFuture<O.Value> { eventLoop.makeFailedFuture(SSHChannelError.unsupported) }
+    
+    // NO-OPS
+    public func flush0() {}
+    public func channelRead0(_ data: NIOAny) {}
+    public func read0() {}
+
+    // Accessors
+    public var allocator: ByteBufferAllocator { session.channel.allocator }
+    public var closeFuture: EventLoopFuture<Void> { closePromise.futureResult }
+    public var parent: Channel? { session.channel }
+    public var _channelCore: ChannelCore { self }
+    public var eventLoop: EventLoop { session.channel.eventLoop }
+    public var pipeline: ChannelPipeline { _pipeline }
+    
+    // State getters
+    public var isWritable: Bool { true }
+    public var isActive: Bool { true }
+    
+    /// Registration callback - succeed if open, fail if closed, quite simple really.
+    public func register0(promise: EventLoopPromise<Void>?) {
+        promise?.completeWith(self.isOpen ? .success(()) : .failure(SSHChannelError.isClosed))
     }
     
-    func remoteAddress0() throws -> SocketAddress {
-        throw SSHChannelError.unsupported
-    }
-    
-    func register0(promise: EventLoopPromise<Void>?) {
-        guard isOpen else {
-            promise?.fail(SSHChannelError.isClosed)
-            return
-        }
-        
-        promise?.succeed(())
-    }
-    
-    func bind0(to: SocketAddress, promise: EventLoopPromise<Void>?) {
-        promise?.fail(SSHChannelError.unsupported)
-    }
-    
-    func connect0(to: SocketAddress, promise: EventLoopPromise<Void>?) {
-        promise?.fail(SSHChannelError.unsupported)
-    }
-    
-    func write0(_ data: NIOAny, promise: EventLoopPromise<Void>?) {
+    /// Send outoging data
+    public func write0(_ data: NIOAny, promise: EventLoopPromise<Void>?) {
         write(unwrapData(data)).cascade(to: promise)
     }
     
-    // NO-OPS
-    func flush0() { }
-    func channelRead0(_ data: NIOAny) { }
-    func errorCaught0(error: Error) { }
-    func read0() {}
-    
-    func close0(error: Error, mode: CloseMode, promise: EventLoopPromise<Void>?) {
+    /// Close channel
+    public func close0(error: Error, mode: CloseMode, promise: EventLoopPromise<Void>?) {
         switch mode {
         case .all:
             self.close().cascade(to: promise)
@@ -123,34 +164,29 @@ extension SSHForwardedChannel: Channel, ChannelCore {
         }
     }
     
-    func triggerUserOutboundEvent0(_ event: Any, promise: EventLoopPromise<Void>?) {
-        promise?.fail(SSHChannelError.unsupported)
+    /// Handle error
+    public func errorCaught0(error: Error) {
+        /// - TODO: Maybe log something with a `Logger` if there isn't any more opportune place to catch these?
     }
-    
-    var allocator: ByteBufferAllocator { session.channel.allocator }
-    var closeFuture: EventLoopFuture<Void> { closePromise.futureResult }
-    
-    var pipeline: ChannelPipeline { _pipeline }
-    var localAddress: SocketAddress? { nil }
-    var remoteAddress: SocketAddress? { nil }
-    
-    var parent: Channel? { session.channel }
-    
-    func setOption<Option>(_ option: Option, value: Option.Value) -> EventLoopFuture<Void> where Option : ChannelOption {
-        return eventLoop.makeFailedFuture(SSHChannelError.unsupported)
-    }
-    
-    func getOption<Option>(_ option: Option) -> EventLoopFuture<Option.Value> where Option : ChannelOption {
-        return eventLoop.makeFailedFuture(SSHChannelError.unsupported)
-    }
-    
-    var isWritable: Bool { true }
-    var isActive: Bool { true }
-    var _channelCore: ChannelCore { self }
-    var eventLoop: EventLoop { session.channel.eventLoop }
 }
 
+// MARK: - SSHSession fowarding channel creation
+
+/// The type of SSH channel to use for forwarding.
+/// - TODO: Is this a correct description?
+enum SSHChannelType: String {
+
+    /// TCP/IP forwarding channel
+    case tcpIpForward = "tcpip-forward"
+    
+    /// Direct TCP/IP connection
+    case directTcpIp = "direct-tcpip"
+}
+
+
 extension SSHSession {
+    /// Crete and configure an `SSHForwardedChannel` of the specified type using
+    /// the provided data as the channel-open request.
     internal func openChannel(
         type: SSHChannelType,
         data: ByteBuffer
@@ -158,168 +194,51 @@ extension SSHSession {
         let openRequest = SSHChannelRequest(
             channelType: type,
             senderId: senderId,
-            windowSize: (2*1024*1024),
-            maxPacketSize: 32768,
+            windowSize: SSH_CHANNEL_WINDOW_SIZE,
+            maxPacketSize: SSH_CHANNEL_MAX_PACKET_SIZE,
             channelData: data
         )
+        senderId &+= 1
         
-        senderId = senderId &+ 1
+        let promise: EventLoopPromise<SSHChannelOpenChannelInfo> = channel.eventLoop.makePromise()
         
-        let promise: EventLoopPromise<SSHChannelResponse> = channel.eventLoop.makePromise()
-        
-        func handle(packet: Result<SSHPacket, Error>) -> Bool {
-            switch packet {
-            case .success(let packet):
-                guard let response = SSHChannelResponse(parsing: packet) else {
-                    promise.fail(SSHError.protocolError)
-                    return true
-                }
-                promise.succeed(response)
-            case .failure(let error):
-                promise.fail(error)
-            }
-            
-            return true
-        }
+        // ByteBuffer needing mutability is the one and only reason this can't be WAY more elegant
+        // (Well, the trinary decision point for the packet type doesn't help either.)
+        func handle(packet: Result<SSHPacket, Error>) -> Bool { promise.accept(packet.flatMap { SSHChannelOpenChannelInfo.parseResponse(packet: $0) }) }
         
         self.context.handlers[.channelOpenConfirm] = handle
         self.context.handlers[.channelOpenFailure] = handle
         
         return channel.writeAndFlush(SSHClientMessage.openChannel(openRequest)).flatMap {
             promise.futureResult
-        }.flatMapThrowing { result in
-            switch result {
-            case .success(let success):
-                let channel = SSHForwardedChannel(
-                    session: self,
-                    state: .open(success)
-                )
-                self.context.channels[success.recipientId] = { [weak channel] buffer in
-                    channel?.read(buffer)
-                }
-                
-                return channel
-            case .failure(let failure):
-                throw failure
-            }
+        }.map {
+            let channel = SSHForwardedChannel(session: self, state: .open($0))
+            
+            self.context.channels[$0.recipientId] = { [weak channel] in channel?.read($0) }
+            return channel
         }
     }
     
-    func forward(
+    /// Outward-facing interface to channel forwarding.
+    /// - TODO: Should this actually be public? It seemed like it but I could have misunderstood.
+    public func forward(
         remoteHost: String,
         remotePort: UInt16,
         connectedIpAddress: String,
         connectedPort: UInt16
     ) -> EventLoopFuture<SSHForwardedChannel> {
+        // Make sure our config params make sense. It's very poor form to potentially assert only here,
+        // which may never get called during normal operation, but it seems the best available option
+        // for the time being. The assertions should never fire as long as the values are kept reasonable.
+        // How sadly convoluted.
+        withExtendedLifetime(_assert_sanity, { assert($0 == ()) })
+    
         var buffer = channel.allocator.buffer(capacity: 100)
         buffer.writeSSH2String(remoteHost)
         buffer.writeInteger(UInt32(remotePort))
         buffer.writeSSH2String(connectedIpAddress)
         buffer.writeInteger(UInt32(connectedPort))
         
-        return openChannel(type: .directTcpIp, data: buffer).map { channel in
-            return channel
-        }
-    }
-}
-
-enum SSHChannelType: String {
-    case tcpIpForward = "tcpip-forward"
-    case directTcpIp = "direct-tcpip"
-}
-
-struct SSHChannelRequest {
-    let channelType: SSHChannelType
-    let senderId: UInt32
-    let windowSize: UInt32
-    let maxPacketSize: UInt32
-    let channelData: ByteBuffer
-}
-
-struct SSHChannelGlobalRequest {
-    let channelType: SSHChannelType
-    let wantsReply: Bool
-    let data: ByteBuffer
-}
-
-enum SSHChannelResponse {
-    struct Success {
-        let recipientId: UInt32
-        let senderId: UInt32
-        let windowSize: UInt32
-        let maxPacketSize: UInt32
-        let channelData: ByteBuffer
-        
-        init?(parsing payload: inout ByteBuffer) {
-            guard
-                let recipientId = payload.readInteger(as: UInt32.self),
-                let senderId = payload.readInteger(as: UInt32.self),
-                let windowSize = payload.readInteger(as: UInt32.self),
-                let maxPacketSize = payload.readInteger(as: UInt32.self),
-                let data = payload.readSlice(length: payload.readableBytes)
-            else {
-                return nil
-            }
-            
-            self.recipientId = recipientId
-            self.senderId = senderId
-            self.windowSize = windowSize
-            self.maxPacketSize = maxPacketSize
-            self.channelData = data
-        }
-    }
-    
-    struct Failure: Error {
-        enum Reason: UInt32 {
-            case prohibited = 1
-            case connectFailed = 2
-            case unknownChannelType = 3
-            case resourceShortage = 4
-        }
-        
-        let recipientId: UInt32
-        let reason: Reason?
-        let description: String
-        let language: String
-        
-        init?(parsing payload: inout ByteBuffer) {
-            guard
-                let recipientId = payload.readInteger(as: UInt32.self),
-                let reason = payload.readInteger(as: UInt32.self),
-                let description = payload.readSSH2String(),
-                let language = payload.readSSH2String()
-            else {
-                return nil
-            }
-            
-            self.recipientId = recipientId
-            self.reason = Reason(rawValue: reason)
-            self.description = description
-            self.language = language
-        }
-    }
-    
-    case success(Success)
-    case failure(Failure)
-    
-    init?(parsing packet: SSHPacket) {
-        var payload = packet.payload
-        
-        switch payload.readInteger(as: UInt8.self) {
-        case SSHPacketType.channelOpenConfirm.rawValue:
-            guard let result = Success(parsing: &payload) else {
-                return nil
-            }
-            
-            self = .success(result)
-        case SSHPacketType.channelOpenFailure.rawValue:
-            guard let result = Failure(parsing: &payload) else {
-                return nil
-            }
-            
-            self = .failure(result)
-        default:
-            return nil
-        }
+        return openChannel(type: .directTcpIp, data: buffer)
     }
 }
---
+++ Sources/Citadel/SSHChannelPacketTypes.swift
@@ -0,0 +1,101 @@
+import NIO
+
+/// The layout of a channel request packet.
+struct SSHChannelRequest {
+    let channelType: SSHChannelType
+    let senderId: UInt32
+    let windowSize: UInt32
+    let maxPacketSize: UInt32
+    let channelData: ByteBuffer
+}
+
+/// The layout of a packet returned from a successful channel request
+struct SSHChannelOpenChannelInfo: Equatable {
+    let recipientId: UInt32
+    let senderId: UInt32
+    let windowSize: UInt32
+    let maxPacketSize: UInt32
+    let channelData: ByteBuffer
+ 
+    /// Parse the packet format from an input buffer if possible.
+    static func from(payload: inout ByteBuffer) -> Result<Self, Error> {
+        guard
+            let recip: UInt32 = payload.readInteger(),
+            let sender: UInt32 = payload.readInteger(),
+            let winSize: UInt32 = payload.readInteger(),
+            let maxPktSize: UInt32 = payload.readInteger(),
+            let data = payload.readSlice(length: payload.readableBytes)
+        else {
+            return .failure(SSHError.protocolError)
+        }
+        return .success(.init(recipientId: recip, senderId: sender, windowSize: winSize, maxPacketSize: maxPktSize, channelData: data))
+    }
+    
+    /// Parse the provided buffer to determine whether the packet it represents,
+    /// if any, is channel open success, channel open failure, or anything else,
+    /// including empty. For success, parse the channel info packet and return
+    /// it as a success result. For failure, parse the error reason packet and
+    /// return it as a failure result. For anything else, or if any error occurs
+    /// in the other cases, return a protocol error failure.
+    ///
+    /// - Note: Why put this method here instead of keeping the
+    ///   `SSHChannelResponse` type, or at least using it as a namespace for
+    ///   this behavior? Answer: This type is the "positive"/"success" packet,
+    ///   which is the place from where actions should generally be sourced when
+    ///   practical and appropriate. From a code architecture standpoint, the
+    ///   "failure" packet is not only a failure condition but also actually
+    ///   conforms to `Error`; such types are not traditionally first- class
+    ///   types in terms of behavior, but rather are usually auxiliary helpers
+    ///   and information couriers. Additionally, this design makes the failure
+    ///   case of our `Result<>` an instance of the failure packet itself, even
+    ///   when parsing it succeeds, while _this_ structure is the success case
+    ///   for all paths. Accordingly, we have reason (if you like) to conclude
+    ///   that this type is sufficiently "authoritative" not to need an
+    ///   enclosing "generic response kind" namespace; there is nothing _other_
+    ///   than the two packet types that might be placed therein.
+    ///
+    /// - Note: The `Result<>` we return has generic `Error` as its failure type
+    ///   instead of the failure packet simply because it makes life easier when
+    ///   chaining actions elsewhere.
+    static func parseResponse(packet: SSHPacket) -> Result<Self, Error> {
+        var payload = packet.payload // copy payload to `var` because `ByteBuffer` is awful that way
+        
+        // Read packet discriinator, convert to packet type if possible. (Doing this as a single step allows
+        // the compiler to infer `UInt8` as the integer type to read so we don't have to specify it.)
+        let type = payload.readInteger().flatMap { SSHPacketType(rawValue: $0) }
+        
+        switch type {
+            case .channelOpenConfirm: return Self.from(payload: &payload) // confirm; try parsing success and return result
+            case .channelOpenFailure: return SSHChannelOpenFailure.from(payload: &payload).flatMap { .failure($0) } // parse fail and make it an error
+            default: return .failure(SSHError.protocolError) // some other packet, or no integer value, return protocol error
+        }
+    }
+}
+
+/// The layout of a packet returned from a failed channel request
+struct SSHChannelOpenFailure: Error, Equatable {
+    enum Reason: UInt32 {
+        case prohibited = 1
+        case connectFailed = 2
+        case unknownChannelType = 3
+        case resourceShortage = 4
+    }
+        
+    let recipientId: UInt32
+    let reason: Reason?
+    let description: String
+    let language: String
+    
+    /// Parse the packet format from an input buffer if possible.
+    static func from(payload: inout ByteBuffer) -> Result<Self, Error> {
+        guard
+            let recip: UInt32 = payload.readInteger(),
+            let reason: UInt32 = payload.readInteger(),
+            let desc = payload.readSSH2String(),
+            let lang = payload.readSSH2String()
+        else {
+            return .failure(SSHError.protocolError)
+        }
+        return .success(.init(recipientId: recip, reason: Reason(rawValue: reason), description: desc, language: lang))
+    }
+}
